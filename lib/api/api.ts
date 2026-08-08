abafa9fbc32e0a620169c3443e6fc94ac71c7f37
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-08-08 16:43:41 +0200

    update api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+const publicAppUrl = process.env.NEXT_PUBLIC_API_URL;
+const vercelAppUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : undefined;
+const appUrl = publicAppUrl ?? vercelAppUrl;
+const baseURL = appUrl ? `${appUrl.replace(/\/$/, "")}/api` : "/api";
 
 export const api = axios.create({
   baseURL,
